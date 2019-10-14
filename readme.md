bbcf0d77af4a6af751e4a7e0e9d8eaa125632a8a
zanjie1999 <zanjie1999@yeah.net>
2019-10-14 23:24:56 +0800

    frist commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# ESP32-CAM 的 RTSP 服务器
+复制wificonf.default.h到wificonf.h设置wifi
---
+++ wificonf.default.h
@@ -0,0 +1,5 @@
+// Rename the file to wificonf.h and edit it for your wifi settings
+int wifiConfNum = 1
+char* wifiConf[wifiConfNum][2] = [
+    ['ssid','password']
+]
